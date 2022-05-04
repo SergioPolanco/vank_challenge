2cf8cf96d95bcea831f679bd17745aef4283b4dc
Sergio Polanco <sipolanco@falabella.cl>
2022-05-04 13:01:43 -0400

    Sergio | static port

--- src/main.ts
+++ src/main.ts
@@ -34,7 +34,7 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(PORT);
+  await app.listen(3000);
 }
 
 bootstrap();
